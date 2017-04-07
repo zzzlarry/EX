0fd8558316b219c09ae994810be32d83a5955615
zzzlarry <105021018@live.asia.edu.tw>
2017-04-07 16:07:35 +0800

    EX

---
+++ src/People.java
@@ -0,0 +1,40 @@
+
+public class People {
+	protected String name, sex, blood;
+	protected float height, weight;
+	public void setName1(String name1) {
+		this.name = name1;
+	}
+
+	public String getName1(String name1) {
+		return name1;
+	}
+	public void setSex(String sex1) {
+		this.sex = sex1;
+	}
+
+	public String getSex(String sex1) {
+		return sex1;
+	}
+	public void setBlood(String blood1) {
+		this.blood = blood1;
+	}
+
+	public String getBlood(String blood1) {
+		return blood1;
+	}
+	public void setHeight(float height1) {
+		this.height = height1;
+	}
+
+	public float getHeight(float height1) {
+		return height1;
+	}
+	public void setWeight(float weight1) {
+		this.weight = weight1;
+	}
+
+	public float getWeight(float weight1) {
+		return weight1;
+	}
+}
---
+++ src/Student.java
@@ -0,0 +1,8 @@
+
+public class Student extends People{
+	public Student(String name,String sex,String blood){
+	name=name1;
+	sex=sex1;
+	blood=blood1;
+	}
+}
